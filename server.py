9adf8d8b36fdf4db4e3d66c743a95b6832718208
VladimirPozdeev06 <vova0306pozdeev@yandex.ru>
2025-12-08 14:55:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import time
+
+
 class Server_company_name:
     def __init__(self):
         self.current_state = 'Waiting_request'
@@ -7,7 +9,7 @@ class Server_company_name:
         self.client_socket = None
         self.client_address = None
         self.current_request = None
-        self.response_data = None
+        self.answer_data = None
         self.process = True
 
     def start_server(self,
@@ -42,9 +44,64 @@ class Server_company_name:
 
         if self.current_state == 'Waiting_request':
             self.func_waiting_request()
-        elif self.current_state == 'PROCESS_REQUEST':
-            self.process_request()
-        elif self.current_state == 'SEND_RESPONSE':
-            self.send_response()
+        elif self.current_state == 'process_editing':
+            self.editing_request()
+        elif self.current_state == 'send_answer':
+            self.send_answer()
         elif self.current_state == 'catch_error':
             self.handle_error()
+
+    def func_waiting_request(self,
+                             time_to_write_request=5.0,
+                             max_length_request=1488,decoder='utf-8',
+                             exception_data=['','sql','c++']):
+
+        if not self.client_socket:
+            try:
+                self.client_socket, self.client_address = self.socket.accept()
+                self.client_socket.settimeout( time_to_write_request)
+                print(f" Подключен: {self.client_address}")
+            except socket.timeout:
+                return
+
+        try:
+            request_from_client = self.client_socket.recv(max_length_request).decode(decoder)
+            if request_from_client not in exception_data:
+                self.current_request = request_from_client.strip().lower()
+                print(f"Получен запрос: '{self.current_request}'")
+                self.current_state = 'process_editing'
+            else:
+                print(" Связь была разорвана")
+                self.client_socket.close()
+                self.client_socket = None
+        except socket.timeout:
+            return
+        except Exception as e:
+            print(f" Ошибка олучения данных: {e}")
+            self.current_state = 'catch_error'
+
+    def editing_request(self):
+
+        try:
+
+
+
+            if self.current_request == "ping":
+                self.answer_data = "pong"
+
+            elif self.current_request == "what is up bro?":
+                self.answer_data = "Hello Client!"
+            elif self.current_request == "goodbye?":
+                self.answer_data = "bye:("
+            else:
+
+                self.answer_data = f"{self.current_request}"
+
+            
+
+
+        except Exception as e:
+            print(f"❌ Ошибка обработки: {e}")
+            self.answer_data = "Ошибка 501"
+        print(f"{self.answer_data}")
+        self.current_state = 'send_answer'
